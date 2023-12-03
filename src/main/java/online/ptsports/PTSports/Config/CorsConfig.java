d9a4bdc3d8de8c457ab6adde7f3875cc36a99bfd
Phuoc <banhluudanhphuoc@gmail.com>
2023-12-03 20:57:21 +0700

    update Cros Config

--- src/main/java/online/ptsports/PTSports/Config/CorsConfig.java
+++ src/main/java/online/ptsports/PTSports/Config/CorsConfig.java
@@ -1,5 +1,5 @@
-
 package online.ptsports.PTSports.Config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -16,6 +16,8 @@ public class CorsConfig {
         config.addAllowedOrigin("https://ptsports.online");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+        config.addExposedHeader("Authorization");
+        config.setAllowCredentials(true); // Allow credentials (e.g., cookies)
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
