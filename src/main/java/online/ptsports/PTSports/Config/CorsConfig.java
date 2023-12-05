2b476d594cbe807289713445a862f0671a0cdcbf
Phuoc <banhluudanhphuoc@gmail.com>
2023-12-05 15:51:13 +0700

    update

--- src/main/java/online/ptsports/PTSports/Config/CorsConfig.java
+++ src/main/java/online/ptsports/PTSports/Config/CorsConfig.java
@@ -6,6 +6,23 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.addAllowedOrigin("https://ptsports.online");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        config.addExposedHeader("Authorization");
+//        config.setAllowCredentials(true); // Allow credentials (e.g., cookies)
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
+//}
+
 @Configuration
 public class CorsConfig {
 
@@ -13,13 +30,16 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");  // Cho phép truy cập từ tất cả các origin
         config.addAllowedOrigin("https://ptsports.online");
+        config.addAllowedOrigin("localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("Authorization");
-        config.setAllowCredentials(true); // Allow credentials (e.g., cookies)
+        config.setAllowCredentials(true);
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 }
+
 
