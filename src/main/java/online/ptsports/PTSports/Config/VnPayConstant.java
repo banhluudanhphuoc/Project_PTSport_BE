a78d26a6fbb07aa1d63b04bd7566be38672bff30
Phuoc <banhluudanhphuoc@gmail.com>
2023-12-07 22:47:33 +0700

    update pay 3

--- src/main/java/online/ptsports/PTSports/Config/VnPayConstant.java
+++ src/main/java/online/ptsports/PTSports/Config/VnPayConstant.java
@@ -11,16 +11,20 @@ import java.time.format.DateTimeFormatter;
 @AllArgsConstructor
 
 public abstract class VnPayConstant {
-    public static String vnp_Version = "2.1.0";
+//    public static String vnp_Version = "2.1.0";
+//    public static String vnp_Command = "pay";
+//    public static String vnp_TmnCode = "0LARC00Y";
+//    public static String vnp_HashSecret = "HAMLOHFBHIHPNQLRWBWCJCIBUTIPXBQR";
+public static String vnp_Version = "2.1.0";
     public static String vnp_Command = "pay";
-    public static String vnp_TmnCode = "0LARC00Y";
-    public static String vnp_HashSecret = "HAMLOHFBHIHPNQLRWBWCJCIBUTIPXBQR";
-//    public static String vnp_CreateDate;
-//    static {
-//        LocalDateTime currentDateTime = LocalDateTime.now(ZoneId.of("GMT+7"));
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-//        vnp_CreateDate = currentDateTime.format(formatter);
-//    }
+    public static String vnp_TmnCode = "3CGWHM1C";
+    public static String vnp_HashSecret = "GYIVPARRHHVGFJJNVXMACDEODYZGYNRE";
+    public static String vnp_CreateDate;
+    static {
+        LocalDateTime currentDateTime = LocalDateTime.now(ZoneId.of("GMT+7"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        vnp_CreateDate = currentDateTime.format(formatter);
+    }
 
     public static String vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
 
@@ -29,6 +33,5 @@ public abstract class VnPayConstant {
     //    public static String vnp_IpAddr = "0:0:0:0:0:0:0:1";
     public static String vnp_Locale = "vn";
     public static String vnp_ReturnUrl = "https://ptsports.online/confirmation";
-    //    public static String vnp_ReturnUrl = "http://localhost:3000/confirmation";
 
 }
